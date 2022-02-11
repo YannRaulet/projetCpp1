6ffdc68b58993bfc2c627ec88084c6bbe3d0a6b5
Yann Raulet <yann.raulet@hotmail.fr>
2022-02-11 09:49:06 +0100

    addition of the private and professional contact list

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDateTime>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +19,40 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pbPrivate_clicked()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    //db.setDatabaseName("/home/yann/Documents/formationCPlusPlus/projetCPP/projetCPP/dbContacts.db");
+    db.setDatabaseName("../projetCPP/dbContacts.db");
+
+
+    QSqlTableModel *model = new QSqlTableModel();
+
+    model->setTable("contacts");
+    model->setFilter("entreprise is null");
+    model->select();
+
+    ui->tableView->setModel(model);
+    ui->tableView->hideColumn(4);
+    //db.close();
+}
+
+void MainWindow::on_pbPro_clicked()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    //db.setDatabaseName("/home/yann/Documents/formationCPlusPlus/projetCPP/projetCPP/dbContacts.db");
+    db.setDatabaseName("../projetCPP/dbContacts.db");
+
+
+    QSqlTableModel *model = new QSqlTableModel();
+
+    model->setTable("contacts");
+    model->setFilter("entreprise is not null");
+    model->select();
+
+    ui->tableView->setModel(model);
+    ui->tableView->showColumn(4);
+
+    //db.close();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pbPrivate_clicked();
+
+    void on_pbPro_clicked();
 
 private:
     Ui::MainWindow *ui;
